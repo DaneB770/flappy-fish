50f9f723165f256cc9569b69c2c2d482a7d45792
DaneB770 <danebacas@gmail.com>
2025-05-03 20:09:33 +0800

    Add files via upload

--- main.js
+++ main.js
@@ -281,7 +281,7 @@ window.addEventListener('offline', () => {
 // Service Worker registration
 if ('serviceWorker' in navigator) {
   window.addEventListener('load', () => {
-    navigator.serviceWorker.register('service-worker.js')
+    navigator.serviceWorker.register('/hombrebueno-finalexam-fluffyfishflappy-fish/service-worker.js')
       .then(reg => console.log('Service Worker registered', reg))
       .catch(err => console.log('Service Worker registration failed', err));
   });
